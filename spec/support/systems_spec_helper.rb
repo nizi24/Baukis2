7e2725b20c9fe137ea3732147828eb2b4c3314b3
nizi <u.nishida24@gmail.com>
2020-05-28 00:31:53 +0000

    complete book2-cp9

--- spec/support/systems_spec_helper.rb
+++ spec/support/systems_spec_helper.rb
@@ -13,4 +13,13 @@ module SystemsSpecHelper
       click_button "ログイン"
     end
   end
+
+  def login_as_customer(customer, password = "pw")
+    visit customer_login_path
+    within("#login-form") do
+      fill_in "メールアドレス", with: customer.email
+      fill_in "パスワード", with: password
+      click_button "ログイン"
+    end
+  end
 end
